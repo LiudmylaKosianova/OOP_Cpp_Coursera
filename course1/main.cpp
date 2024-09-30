8a0ee6d0f66765e14635295baa298b0cc8d668e8
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-30 19:10:44 +0200

    menu options

---
+++ course1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    //menu options
+    cout << "1: Print help" << endl;
+    cout << "2: Print exchange stats" << endl;
+    cout << "3: Place an offer" << endl;
+    cout << "4: Place a bid" << endl;
+    cout << "5: Print wallet" << endl;
+    cout << "6: Continue" << endl;
+    
+    return 0;
+}
