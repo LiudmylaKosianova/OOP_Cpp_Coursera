5b8db31f78b23844caec4d4f4f8c4204423e2639
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-30 19:25:35 +0200

    check user input and take actions

--- course1/main.cpp
+++ course1/main.cpp
@@ -17,5 +17,32 @@ int main()
     cout << "Type in 1-6" << endl;
     cin >> userOption;
 
+    //check user input and take actions
+    switch(userOption)
+    {
+        case 1:
+        cout << "Follow the instructions. Your goal is to make money." << endl;
+        break;
+        case 2:
+        cout << "Your stats are empty." << endl;
+        break;
+        case 3:
+        cout << "Specify what you want to sell." << endl;
+        break;
+        case 4:
+        cout << "Specify what you want to buy." << endl;
+        break;
+        case 5:
+        cout << "Your wallet is empty." << endl;
+        break;
+        case 6:
+        cout << "Moving on." << endl;
+        break;
+        default:
+        cout << "Bad input. Please, choose the number 1-6." << endl;
+        break;
+    }
+    
+
     return 0;
 }
