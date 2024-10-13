fb2a8d635a75880d2f99c2388366ec2528659d4d
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-10-13 13:23:25 +0200

    snowflakes.cpp: classes and an object created

--- course1/snowflakes.cpp
+++ course1/snowflakes.cpp
@@ -4,10 +4,35 @@
 
 enum class OrderBookType{bid, offer};
 
-
+class OrderBookEntry
+{
+    public:
+    OrderBookEntry(double _price,
+                   double _amount,
+                   std::string _timestamp,
+                   std::string _product,
+                   OrderBookType _orderType)
+    {
+        price = _price;
+        amount = _amount;
+        timestamp = _timestamp;
+        product = _product;
+        orderType = _orderType;
+    }
+    double price;
+    double amount;
+    std::string timestamp;
+    std::string product;
+    OrderBookType orderType;
+};
 
 int main()
 {
-
-    return 0;
+    OrderBookEntry entry1(1000,
+                          0.02,
+                          "2020/03/17 17:01:24",
+                          "BTC/USDT",
+                          OrderBookType::bid);
+    std::cout << "The price is " << entry1.price << std::endl;
+    return 0; 
 }
