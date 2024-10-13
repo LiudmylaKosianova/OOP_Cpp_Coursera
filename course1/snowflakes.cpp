b1022fdd3a3231a6df64b29b7d6c70065d3705e1
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-10-13 16:27:55 +0200

    snowflakes.cpp iterating over a vector options

--- course1/snowflakes.cpp
+++ course1/snowflakes.cpp
@@ -37,8 +37,43 @@ int main()
     std::cout << "The price is " << entry1.price << std::endl;
 
     std::vector<OrderBookEntry> orders;
-    orders.push_back(entry1);
+    orders.push_back(OrderBookEntry{
+                          1000,
+                          0.02,
+                          "2020/03/17 17:01:24",
+                          "BTC/USDT",
+                          OrderBookType::bid});
 
-    std::cout << "The price is " << orders[0].price << std::endl;
+    orders.push_back(OrderBookEntry{
+                          3000,
+                          0.02,
+                          "2020/03/17 17:01:24",
+                          "BTC/USDT",
+                          OrderBookType::bid});
+
+    //iterator style makes copies 
+    for(OrderBookEntry order : orders)
+    {
+        std::cout << "The price is " << order.price << std::endl;
+    }
+
+    //iterator style does not make copies (iterate by reference)
+    for(OrderBookEntry& order : orders)
+    {
+        std::cout << "The price is " << order.price << std::endl;
+    }
+
+    //array style
+    for(unsigned int i = 0; i < orders.size(); ++i)
+    {
+        std::cout << "The price is " << orders[i].price << std::endl;
+    }
+
+    //object style 
+    for(unsigned int i = 0; i < orders.size(); ++i)
+    {
+        std::cout << "The price is " << orders.at(i).price << std::endl;
+    }
+
     return 0; 
 }
