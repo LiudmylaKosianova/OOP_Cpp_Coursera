563bc92c3827ce46726fd7bbc0dbbba2b1b708c1
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-10-15 14:36:22 +0200

    OrderBookEntry header file added

---
+++ OrderBookEntry.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+
+enum class OrderBookType{bid, ask};
+
+class OrderBookEntry
+{
+    public:
+
+    //constructor
+    OrderBookEntry(std::string _timestamp,
+                   std::string _product,
+                   OrderBookType _orderType,
+                   double _price,
+                   double _amount);
+
+    //data types to represent a the fields in a row in the data file
+    double price;
+    double amount;
+    std::string timestamp;
+    std::string product;
+    OrderBookType orderType;
+};
