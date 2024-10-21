e7dd618bcb1cadd227e6ecc5bf01f45f8471adec
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-10-21 16:10:47 +0200

    tokenise function dowhile loop in progress

--- token_alg_test.cpp
+++ token_alg_test.cpp
@@ -2,9 +2,19 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> tokenise()
+std::vector<std::string> tokenise(std::string csvLine, char separator)
 {
+    std::vector<std::string> tokens;
 
+    int start, end;
+    start = csvLine.find_first_not_of(separator, 0);
+    do
+    {
+        end = csvLine.find_first_of(separator, start);
+        if(start == csvLine.length()||start == end) break;
+
+    }while();
+    return tokens;
 }
 
 int main()
