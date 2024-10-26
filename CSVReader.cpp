056c8c4e0ad280c31fc22904f552d7ac385591c0
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-10-26 12:31:55 +0200

    function stringsToOBe in progress

--- CSVReader.cpp
+++ CSVReader.cpp
@@ -15,8 +15,23 @@ vector<string> CSVReader::tokenise(string csvLine, char separator)
     return tokens;
 }
 
-OrderBookEntry CSVReader::stringsToOBE(vector<string> strings)
+OrderBookEntry CSVReader::stringsToOBE(vector<string> tokens)
 {
+    double price, amount;
+
+    if(tokens.size() != 5)
+    {
+        throw std::exception{};
+    }
+            
+    try
+    {
+        price = std::stod(tokens[3]);
+        amount = std::stod(tokens[4]);
+    }
+    catch(const std::exception& e)
+    {throw;}
+    
     OrderBookEntry obe{"","", OrderBookType::bid,1.0,1.0};
     return obe;
 }
