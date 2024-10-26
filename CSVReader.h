6a35004540f3075093209ab756a0a1830481d4e4
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-10-26 08:59:07 +0200

    CSVReader header file created

---
+++ CSVReader.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "OrderBookEntry.h"
+
+using std::string;
+using std::vector;
+
+class CSVReader
+{
+    public:
+    CSVReader();
+
+    static vector<OrderBookEntry> readCSV(string CSVFile);
+
+    private:
+    static vector<string> tokenise(string csvLine, char separator);
+};
