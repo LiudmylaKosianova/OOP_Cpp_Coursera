4fc3bb8095c3faf6b05cc0cfb2ca4d5aca93858c
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-11-17 14:15:14 +0100

    insertOrder() function started

--- OrderBook.h
+++ OrderBook.h
@@ -13,26 +13,34 @@ class OrderBook
     public:
     /**construct, reading a csv data file */
     OrderBook(std::string filename);
+
+
     /**return vector of all known products in the dataset */
     std::vector<std::string> getKnownProduct();
+
     /**return vector of orders according to the filters */
     std::vector<OrderBookEntry> getOrders(OrderBookType type,
                                         std::string product,
                                         std::string timestamp); 
-    /**return the highest price */
+
+    /**returns the highest price */
     static double getHighPrice(std::vector<OrderBookEntry>& orders);
 
+    /**returns the lowest price */
     static double getLowPrice(std::vector<OrderBookEntry>& orders);
+
     /**returns the earliest time in the order book */
     std::string getEarliestTime();
 
-    /**return the total amount of a product */
+    /**returns the total amount of a product */
     static double getAmount(std::vector<OrderBookEntry>& orders);
 
     /**returns the next time after the specified in the argument time,
      * if there is no next time, returns the earliest time
      */
     std::string getNextTime(std::string timestamp);
+
+    void insertOrder(OrderBookEntry& order);
 
     private:
     std::vector<OrderBookEntry> orders;
