f94b47bdd57d0f268487a71fe328941f45866890
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-11-18 15:00:14 +0100

    class Wallet header file finished

--- Wallet.h
+++ Wallet.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <string>
 #include <iostream>
 
 class Wallet
@@ -8,6 +9,13 @@ class Wallet
     public:
     Wallet();
 
+    /**puts currency into the wallet */
+    void insertCurrency(std::string type, double amount);
+    /**checks if there is the enough amount of currency  */
+    bool constainsCurrency(std::string type, double amount);
+    /**generates a string representation of a wallet */
+    std::string toString;
+
     private:
-    std::map<std::string, double> currency;
+    std::map<std::string, double> currencies;
 };
